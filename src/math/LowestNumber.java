e548f6ec2345ee6b6dbdd47fafec9409f35cd80d
lilinyc <wirelesspremium@gmail.com>
2017-04-27 00:47:29 -0400

    implemented lowest number

--- src/math/LowestNumber.java
+++ src/math/LowestNumber.java
@@ -16,18 +16,29 @@ public class LowestNumber {
 
 		ConnectDB connectDB = new ConnectDB();
 
-		List<String> lowestValue = new ArrayList<String>();
+		List<String> lowestValues = new ArrayList<String>();
 		try {
 			connectDB.InsertDataFromArryToMySql(array, "tbl_lowestNumber", "column_lowestNumber");
-			lowestValue = connectDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+			lowestValues = connectDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-		for(String st:lowestValue){
-			System.out.println(st);
+		System.out.println(lowestValues.toString());
+		Integer lowestNumber = null;
+		for(String st:lowestValues){
+			Integer number = Integer.parseInt(st);
+			if(lowestNumber==null){
+				lowestNumber = number;
+			}else{
+				if(number<lowestNumber){
+					lowestNumber = number;
+				}
+			}
 		}
+
+		System.out.println("Lowest number is: " + lowestNumber);
 	}
 
 }
