a4f4f081342e81210b37254de05134b46505cfde
lilinyc <wirelesspremium@gmail.com>
2017-04-26 22:59:28 -0400

    implemented permutation

--- src/problems/Permutation.java
+++ src/problems/Permutation.java
@@ -1,9 +1,26 @@
 package problems;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class Permutation {
+    public static LinkedList<String> getPermutations(String input) {
+        LinkedList<String> result = new LinkedList<String>();
+        if (input.length() <= 1) {
+            result.add(input);
+        } else {
+            for (Character c: input.toCharArray()) {
+                LinkedList<String> tempResults = getPermutations(input.replaceFirst(c.toString(), ""));
+                for (String tempResult : tempResults) {
+                    result.add(c + tempResult);
+                }
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
 
@@ -12,5 +29,9 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+
+        LinkedList<String> result = getPermutations("ABC");
+        System.out.println("The string representation of array is:");
+        System.out.println(result.toString());
     }
 }
